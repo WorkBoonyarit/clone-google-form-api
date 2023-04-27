8364bc5e0a3670ccdbdfcab2ad9140ecd07edc46
Jay.boonyarit <works.boonyarit@gmail.com>
2023-04-27 20:58:24 +0700

    change type data structure

--- models/polls.models.ts
+++ models/polls.models.ts
@@ -4,11 +4,11 @@ import { db } from "../bootstrapDatabase";
 export const pollsModel = db.define('gf_polls', {
     id: { type: Sequelize.INTEGER, primaryKey: true },
     name: { type: Sequelize.STRING },
-    type_ins: { type: Sequelize.INTEGER },
-    type_topup: { type: Sequelize.INTEGER },
-    type_fixed: { type: Sequelize.INTEGER },
-    type_year: { type: Sequelize.INTEGER },
-    type_fix: { type: Sequelize.INTEGER },
+    type_ins: { type: Sequelize.STRING },
+    type_topup: { type: Sequelize.STRING },
+    type_fixed: { type: Sequelize.STRING },
+    type_year: { type: Sequelize.STRING },
+    type_fix: { type: Sequelize.STRING },
     range_sum_insure: { type: Sequelize.INTEGER },
     range_premiums_insure: { type: Sequelize.INTEGER },
 }, {
